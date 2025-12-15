fdb4594b22efa3dd836d051a9b2e597cd062c270
PyCSharp <210414190+PyCSharp@users.noreply.github.com>
2025-12-15 20:56:26 +0100

    Add ControlRegister enum and related functions

--- src/MemLib.h
+++ src/MemLib.h
@@ -4,11 +4,24 @@
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
+#include <string>
 
 // This is just the header file, look into MemLib.cpp for the code
+
+enum class ControlRegister {
+    VBR,
+    SSR,
+    SPC,
+    SGR,
+    DBR,
+    GBR,
+    SR,
+};
 
 std::vector<char> ReadMemory(uintptr_t addr, size_t size);
 void WriteMemory(uintptr_t addr, const void* data, size_t size);
 void ExecuteFromAddr(uintptr_t addr);
 void ExecuteShellcode(unsigned char* code);
 void Sleep(int sec);
+uint32_t ReadControlRegister(ControlRegister controlRegister);
+void WriteControlRegister(ControlRegister controlRegister, uint32_t value);
